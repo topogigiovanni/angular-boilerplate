5c236fd62f78031dcc6a572688b7fbbccb28958f
Giovanni Mendes Mansueto <topogigiovanni@gmail.com>
2016-10-10 23:02:35 -0300

    first commit

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,6 +1,6 @@
 const AppSettings = {
   appTitle: 'Example Application',
-  apiUrl: '/api/v1'
+  apiUrl: 'http://private-74b50-provaangularjs.apiary-mock.com/movies'
 };
 
 export default AppSettings;
--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@ import constants from './constants';
 import onConfig  from './on_config';
 import onRun     from './on_run';
 import 'angular-ui-router';
+import 'angular-ui-bootstrap';
 import './templates';
 import './filters';
 import './controllers';
@@ -14,6 +15,7 @@ import './directives';
 // create and bootstrap application
 const requires = [
   'ui.router',
+  'ui.bootstrap',
   'templates',
   'app.filters',
   'app.controllers',
